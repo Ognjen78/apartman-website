d25716541b60a065adb9270dc1c447ce6063df72
Ognjen78 <ognjenmare88@gmail.com>
2025-04-21 13:58:04 +0200

    initial

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-green-500 text-white text-center p-4 rounded-xl shadow-lg">
+  Ovo je Tailwind komponenta
+</div>
+
+  );
+}
+
+export default App;
